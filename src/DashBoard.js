675f361080b9cfc598a071e568c2932aba232503
shubhamsuri <shubhamsuri@lambdatest.com>
2022-06-15 13:54:24 +0530

    update code

---
+++ src/DashBoard.js
@@ -0,0 +1,174 @@
+import {
+    ClockIcon,
+    UsersIcon,
+    ShieldCheckIcon,
+    ShareIcon,
+    CodeIcon,
+    TrendingUpIcon,
+    LinkIcon,
+    BanIcon,
+    PencilIcon
+} from '@heroicons/react/outline'
+import { useState } from 'react'
+import BlockForm from './BlockForm'
+import CorsForm from './CorsForm'
+import CspForm from './CspForm'
+import RedirectionForm from './RedirectionForm'
+import ThrottleForm from './ThrottleForm'
+import UserAgent from './UserAgent'
+
+
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+}
+
+
+
+export default function DashBoard() {
+    const [flag, setFlag] = useState(false);
+    const [Xyz, setXyz] = useState(false);
+    const abc = ()=>{
+        setFlag(true)
+        setXyz(false)
+
+    }
+    const xyz = ()=>{
+        setXyz(true)
+        setFlag(false)
+
+    }
+    const actions = [
+        {
+            title: 'Modify Headers',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            click: abc,
+            icon: PencilIcon,
+            iconForeground: 'text-sky-700',
+            iconBackground: 'bg-sky-50',
+        },
+        {
+            title: 'Block Requests',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            click: xyz,
+            icon: BanIcon,
+            iconForeground: 'text-rose-700',
+            iconBackground: 'bg-rose-50',
+        },
+        {
+            title: 'Throttle Response',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            href: '#',
+            icon: ClockIcon,
+            iconForeground: 'text-yellow-700',
+            iconBackground: 'bg-yellow-50',
+        },
+        {
+            title: 'Add/Remove Query Param',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            href: '#',
+            icon: LinkIcon,
+            iconForeground: 'text-[#111827]',
+            iconBackground: 'bg-[#F0F9FF]'
+        },
+        {
+            title: 'Redirect Requests',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            href: '#',
+            icon: TrendingUpIcon,
+            iconForeground: 'text-[#EAB308]',
+            iconBackground: 'bg-yellow-50',
+        },
+        {
+            title: 'Change User Agent',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            href: '#',
+            icon: UsersIcon,
+            iconForeground: 'text-indigo-700',
+            iconBackground: 'bg-indigo-50',
+        },
+        {
+            title: 'Insert Scripts (CSS/JS)',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            href: '#',
+            icon: CodeIcon,
+            iconForeground: 'text-[#111827]',
+            iconBackground: 'bg-[#F0F9FF]'
+        },
+        {
+            title: 'Allow CORS',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            href: '#',
+            icon: ShareIcon,
+            iconForeground: 'text-green-700',
+            iconBackground: 'bg-green-50',
+        },
+        {
+            title: 'Content Security Policy',
+            description: 'Modify HTTP headers in request and response. Use this tool if you need to modify authentication tokens, remove X-Frame-Options etc.',
+            href: '#',
+            icon: ShieldCheckIcon,
+            iconForeground: 'text-[#7E22CE]',
+            iconBackground: 'bg-[#FAF5FF]',
+        }
+    ]
+    return (
+
+        <div className="rounded-lg bg-gray-200 overflow-hidden shadow divide-y divide-gray-200 sm:divide-y-0 sm:grid sm:grid-cols-3 sm:gap-px px-50">
+            
+          {flag &&  <BlockForm />}
+        {Xyz && <ThrottleForm /> }
+
+            {/* <RedirectionForm /> */}
+            {/* <UserAgent /> */}
+
+            {/* <CorsForm /> */}
+            {/* <CspForm /> */}
+            {actions.map((action, actionIdx) => (
+                <div
+                    key={action.title}
+                    className={classNames(
+                        actionIdx === 0 ? 'rounded-tl-lg rounded-tr-lg sm:rounded-tr-none' : '',
+                        actionIdx === 1 ? 'sm:rounded-tr-lg' : '',
+                        actionIdx === actions.length - 2 ? 'sm:rounded-bl-lg' : '',
+                        actionIdx === actions.length - 1 ? 'rounded-bl-lg rounded-br-lg sm:rounded-bl-none' : '',
+                        'relative group bg-white px-[1vw] py-[1vw] focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500'
+                    )}
+                >
+                    <div>
+                        <span
+                            className={classNames(
+                                action.iconBackground,
+                                action.iconForeground,
+                                'rounded-lg inline-flex p-3 ring-4 ring-white'
+                            )}
+                        >
+                            <action.icon className="h-[1.7vw] w-[1.7vw]" aria-hidden="true" />
+                        </span>
+                    </div>
+                    <div className="mt-[1.6vw]">
+                        <h3 className="text-[1.3vw] font-medium">
+                            <div index={action.index} onClick={action.click} className="focus:outline-none">
+                                {/* Extend touch target to entire panel */}
+                                <span className="absolute inset-0" aria-hidden="true" />
+                                {action.title}
+                            </div>
+                        </h3>
+                        <p className="mt-2 text-[1vw] font-normal text-[#6B7280] leading-tight">
+                            {action.description}
+                        </p>
+                    </div>
+                    <span
+                        className="pointer-events-none absolute top-6 right-[1vw] text-gray-300 group-hover:text-gray-400"
+                        aria-hidden="true"
+                    >
+                        <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                            <path d="M20 4h1a1 1 0 00-1-1v1zm-1 12a1 1 0 102 0h-2zM8 3a1 1 0 000 2V3zM3.293 19.293a1 1 0 101.414 1.414l-1.414-1.414zM19 4v12h2V4h-2zm1-1H8v2h12V3zm-.707.293l-16 16 1.414 1.414 16-16-1.414-1.414z" />
+                        </svg>
+                    </span>
+                </div>
+            ))}
+        </div>
+
+    )
+}
---
+++ src/SideBar.js
@@ -0,0 +1,213 @@
+import { Fragment, useState } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import {
+  BellIcon,
+  CogIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  MenuAlt2Icon,
+  SparklesIcon,
+  XIcon,
+} from '@heroicons/react/outline'
+import { SearchIcon } from '@heroicons/react/solid'
+import DashBoard from './DashBoard'
+
+const navigation = [
+  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
+  { name: 'Manage Rules', href: '#', icon: LightningBoltIcon, current: false },
+  { name: 'More Tools', href: '#', icon: SparklesIcon, current: false },
+  { name: 'Settings', href: '#', icon: CogIcon, current: false }
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function Sidebar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+
+  return (
+    <>
+      {/*
+        This example requires updating your template:
+
+        ```
+        <html class="h-full bg-gray-100">
+        <body class="h-full">
+        ```
+      */}
+      <div>
+        <Transition.Root show={sidebarOpen} as={Fragment}>
+          <Dialog as="div" className="relative z-40 md:hidden" onClose={setSidebarOpen}>
+            <Transition.Child
+              as={Fragment}
+              enter="transition-opacity ease-linear duration-300"
+              enterFrom="opacity-0"
+              enterTo="opacity-100"
+              leave="transition-opacity ease-linear duration-300"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+            >
+              <div className="fixed inset-0 bg-gray-600 bg-opacity-75" />
+            </Transition.Child>
+
+            <div className="fixed inset-0 flex z-40">
+              <Transition.Child
+                as={Fragment}
+                enter="transition ease-in-out duration-300 transform"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition ease-in-out duration-300 transform"
+                leaveFrom="translate-x-0"
+                leaveTo="-translate-x-full"
+              >
+                <Dialog.Panel className="relative flex-1 flex flex-col max-w-xs w-full pt-5 pb-4 bg-gray-800">
+                  <Transition.Child
+                    as={Fragment}
+                    enter="ease-in-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in-out duration-300"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
+                  >
+                    <div className="absolute top-0 right-0 -mr-12 pt-2">
+                      <button
+                        type="button"
+                        className="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        onClick={() => setSidebarOpen(false)}
+                      >
+                        <span className="sr-only">Close sidebar</span>
+                        <XIcon className="h-6 w-6 text-white" aria-hidden="true" />
+                      </button>
+                    </div>
+                  </Transition.Child>
+                  <div className="flex-shrink-0 flex items-center px-4">
+                    <img
+                      className="h-8 w-auto brightness-0 invert"
+                      src="https://www.lambdatest.com/resources/images/lt_logo.png"
+                      alt="LambdaTest"
+                    />
+                  </div>
+                  <div className="mt-5 flex-1 h-0 overflow-y-auto">
+                    <nav className="px-2 space-y-1">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current
+                              ? 'bg-gray-900 text-white'
+                              : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                            'group flex items-center px-2 py-2 text-base font-medium rounded-md'
+                          )}
+                        >
+                          <item.icon
+                            className={classNames(
+                              item.current ? 'text-gray-300' : 'text-gray-400 group-hover:text-gray-300',
+                              'mr-4 flex-shrink-0 h-6 w-6'
+                            )}
+                            aria-hidden="true"
+                          />
+                          {item.name}
+                        </a>
+                      ))}
+                    </nav>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+              <div className="flex-shrink-0 w-14" aria-hidden="true">
+                {/* Dummy element to force sidebar to shrink to fit close icon */}
+              </div>
+            </div>
+          </Dialog>
+        </Transition.Root>
+
+        {/* Static sidebar for desktop */}
+        <div className="hidden md:flex md:w-[7.8vw] md:flex-col md:fixed md:inset-y-0">
+          {/* Sidebar component, swap this element with another sidebar if you like */}
+          <div className="flex-1 flex flex-col min-h-0 bg-[#2E3D56]">
+            <div className="flex items-center h-24 flex-shrink-0 px-4">
+              <img
+                className="h-10 mx-auto w-auto brightness-0 invert"
+                src="https://www.lambdatest.com/resources/images/lt_logo.png"
+                alt="LambdaTest"
+              />
+            </div>
+            <div className="flex-1 flex flex-col overflow-y-auto">
+              <nav className="flex-1 px-2 py-3">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className={classNames(
+                      item.current ? 'bg-[#0EBAC6] text-white' : 'text-white hover:bg-[#0ebac6] hover:text-white',
+                      'group block text-center px-[.3vw] py-2 mb-4 text-[.9vw] font-medium rounded-md'
+                    )}
+                  >
+                    <item.icon
+                      className={classNames(
+                        item.current ? 'text-white' : 'text-white group-hover:text-white',
+                        'flex-shrink-0 h-[1.7vw] w-[1.7vw] mx-auto my-[5px]'
+                      )}
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                  </a>
+                ))}
+              </nav>
+            </div>
+          </div>
+        </div>
+        <div className="md:pl-[7.8vw] flex flex-col">
+          <div className="sticky top-0 z-10 flex-shrink-0 flex h-14 bg-white shadow">
+            <button
+              type="button"
+              className="px-4 border-r border-gray-200 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
+              onClick={() => setSidebarOpen(true)}
+            >
+              <span className="sr-only">Open sidebar</span>
+              <MenuAlt2Icon className="h-6 w-6" aria-hidden="true" />
+            </button>
+            <div className="flex-1 px-4 flex justify-between">
+              <div className="flex-1 flex">
+                <form className="w-full flex md:ml-0" action="#" method="GET">
+                  <label htmlFor="search-field" className="sr-only">
+                    Search
+                  </label>
+                  <div className="relative w-full text-gray-400 focus-within:text-gray-600">
+                    <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
+                      <SearchIcon className="h-5 w-5" aria-hidden="true" />
+                    </div>
+                    <input
+                      id="search-field"
+                      className="block w-full h-full pl-8 pr-3 py-2 border-transparent text-gray-900 placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-0 focus:border-transparent sm:text-sm"
+                      placeholder="Search"
+                      type="search"
+                      name="search"
+                    />
+                  </div>
+                </form>
+              </div>
+              <div className="ml-4 flex items-center md:ml-6">
+                <button
+                  type="button"
+                  className="z-20 bg-white p-1 rounded-full text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <BellIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+
+                
+              </div>
+            </div>
+          </div>
+
+          <main className="flex-1 p-[1vw]">
+            <DashBoard />
+          </main>
+        </div>
+      </div>
+    </>
+  )
+}
